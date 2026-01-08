589d749c38691c371b7e9098d4945643685ec02d
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-01-08 11:31:46 -0500

    Refactor server start method with database connection

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
 const app = require("./src/app");
-const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-})
+const PORT = process.env.PORT || 5000;
+const dbURI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(dbURI);
+    console.log('MongoDB connected');
+
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    })
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+}
+
+startServer();
--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+app.use(express.json());
 
 app.get('', (req, res) => {
   return res.send('Hello Wold');
