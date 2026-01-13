bad2b369de1fe3804a190315c7e1a6fdd0e6d17d
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-01-13 12:26:08 -0500

    Add morgan logger to app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 require('dotenv').config();
 
+// Middleware
+app.use(morgan('dev'));
 app.use(express.json());
 
-app.get('', (req, res) => {
-  return res.send('Hello Wold');
-});
+// Routes
+app.get('', (req, res) => { return res.send('SoporteUAC API') });
+app.use('/users', require('./routes/user.routes'));
 
 module.exports = app;
